0ac081276a3c5d1de1f59380cff63982ace9c768
GabrielArenas <maestromagikarp@gmail.com>
2026-04-22 15:03:50 -0400

    creacion de proyecto y aplicacion

---
+++ sistmea_iujo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sistmea_iujo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistmea_iujo.settings')
+
+application = get_wsgi_application()
